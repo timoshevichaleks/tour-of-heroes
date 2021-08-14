dce0a1e163adea79288bac1007ceb9feca7c1ea7
timoshevichaleks <alti2692@gmail.com>
2021-08-14 13:38:30 +0300

    Add Navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,11 +11,28 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero?: Hero;
+  // @Input() hero?: Hero;
+  hero: Hero | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    // const id = +this.route.snapshot.paramMap.get('id');
+
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+
+    // The route.snapshot is a static image of the route information shortly after the component was created.
+    // The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+    // Route parameters are always strings. The JavaScript Number function converts the string to a number, which is what a hero id should be.
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,9 +12,15 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of (HEROES);
+    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 
 }
